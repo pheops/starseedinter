993ede881dc1b4a10a6b4509cb983b59dafd8959
pheops <92880990+pheops@users.noreply.github.com>
2021-11-29 13:25:19 -0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ let pause = false;
 
 var textures = [];
 
-var totalTextures = 400;
+var totalTextures = 800;
 var loading = true;
 var counter = 0;
 
@@ -102,7 +102,7 @@ function addImg() {
   	//create an if statement
    	// if i is less than 3, add 1 to the value of i
   	// if it's more than that, set the value of i to equal 1
-		if (i<399){
+		if (i<799){
       i++;
     }else{
       i=0;
